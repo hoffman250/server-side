967c243bfe398a9e8eee850c1ec38017db3ef0ec
hoffman250 <hoffman250@gmail.com>
2015-11-13 13:08:10 -0800

    added random function

--- activity2.js
+++ activity2.js
@@ -6,7 +6,7 @@ app.set('port', 3000);
 
 app.get('/',function(req,res){
   res.type('text/plain');
-  res.send("Abigail doesn't like this webpage!");
+  res.send("Testing a random number generator" + math.random());
 });
 
 app.get('/other-page',function(req,res){
